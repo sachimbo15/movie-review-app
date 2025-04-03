2c16246d05dad3ff77c2658d1461adda2015a265
sachimbo15 <shogunato96@gmail.com>
2025-04-02 22:21:57 -0700

    added review django model

--- movie_review_app/reviews/models.py
+++ movie_review_app/reviews/models.py
@@ -4,3 +4,12 @@ class Movie(models.Models):
     title = models.CharField(max_length=255)
     release_year = models.ImageField()
     genre = models.CharField(max_length=100)
+
+class Review(models.Models):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    rating = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
