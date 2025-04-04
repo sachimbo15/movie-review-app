e868a66fcfa98bac09dafa9a0ff684abcae77d09
sachimbo15 <shogunato96@gmail.com>
2025-04-03 23:42:09 -0700

    Added a few titles

--- movie_review_app/reviews/admin.py
+++ movie_review_app/reviews/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 
 from .models import Movie, Review
 
-admin.site.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'release_year', 'genre')
+
+admin.site.register(Movie, MovieAdmin)
 admin.site.register(Review)
+
 # Register your models here.
+
+
