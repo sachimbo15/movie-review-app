367f9dc043ba7f2ddb2f3a4b80f6f70123179f87
sachimbo15 <shogunato96@gmail.com>
2025-04-03 22:46:21 -0700

    Added migrations, register Movie, and Review models

--- movie_review_app/reviews/models.py
+++ movie_review_app/reviews/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-class Movie(models.Models):
+class Movie(models.Model):
     title = models.CharField(max_length=255)
-    release_year = models.ImageField()
+    release_year = models.IntegerField()
     genre = models.CharField(max_length=100)
 
-class Review(models.Models):
+class Review(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     rating = models.IntegerField()
