a1a205a0abbbbf142b2a0cb8ba3ca7d803cc8ae7
sachimbo15 <shogunato96@gmail.com>
2025-04-04 23:05:50 -0700

    Created html for movie_list

--- movie_review_app/reviews/views.py
+++ movie_review_app/reviews/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Movie
 
+def movie_list(request):
+    movies = Movie.objects.all()
+    return render(request, "reviews/movie_list.html", {"movies": movies})
 # Create your views here.
