fc55aee26f4498335faa6e5a72a8b58808550545
sachimbo15 <shogunato96@gmail.com>
2025-04-15 23:00:59 -0700

    routed forms urls in urls.py

--- movie_review_app/movie_review_app/urls.py
+++ movie_review_app/movie_review_app/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from reviews import views  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('movie-list/', views.movie_list, name='movie-list'),
+    path('add-review/', views.add_review, name='add-review'),
+    path('suggest-movie/', views.suggest_movie, name='suggest_movie'),
+
 ]
